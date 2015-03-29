b73166defca91ff78a8ea41b6f0249e9d5a7c008
Brian Woodward <brian.woodward@gmail.com>
2015-03-28 23:57:18 -0400

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * level-revision-sublevel <https://github.com/doowb/level-revision-sublevel>
  *
- * Copyright (c) 2015 Brian Woodward.
- * Licensed under the MIT license.
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
  */
 
 'use strict';
