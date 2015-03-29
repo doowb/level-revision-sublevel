12f902a85e21d8e4a60c31855df72502a255fcca
Brian Woodward <brian.woodward@gmail.com>
2015-03-28 23:56:58 -0400

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * level-revision-sublevel <https://github.com/doowb/level-revision-sublevel>
+ *
+ * Copyright (c) 2015 Brian Woodward.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
